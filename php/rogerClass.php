eef33ead7b19aa51584817945a69430c249d9c23
roger <roger.angel@163.com>
2022-09-19 14:05:44 +0800

    经纬度相关三个方法

--- php/rogerClass.php
+++ php/rogerClass.php
@@ -597,6 +597,39 @@ class rogerClass
         echo $html;
         exit;
     }
+
+
+    /**
+     * 根据经纬度和半径计算出范围
+     * @param string $lat 纬度
+     * @param String $lng 经度
+     * @param float $radius 半径 单位：m
+     * @return Array 范围数组
+     */
+    function CalcScope($lat, $lng, $radius)
+    {
+        $degree = (24901 * 1609) / 360.0;
+        $dpmLat = 1 / $degree;
+
+        $radiusLat = $dpmLat * $radius;
+        $minLat = $lat - $radiusLat;    // 最小纬度
+        $maxLat = $lat + $radiusLat;    // 最大纬度
+
+        $mpdLng = $degree * cos($lat * (pi() / 180));
+        $dpmLng = 1 / $mpdLng;
+        $radiusLng = $dpmLng * $radius;
+        $minLng = $lng - $radiusLng;   // 最小经度
+        $maxLng = $lng + $radiusLng;   // 最大经度
+
+        /** 返回范围数组 */
+        $scope = array(
+            'minLat'  => $minLat,
+            'maxLat'  => $maxLat,
+            'minLng'  => $minLng,
+            'maxLng'  => $maxLng
+        );
+        return $scope;
+    }
 }
 
 //===================================================================测试文件上传 start======================================================================
