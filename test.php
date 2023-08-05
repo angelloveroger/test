a8b825bc9fa10d4d9502d5b0455ff64e95d69b35
angelloveroger <41990016+angelloveroger@users.noreply.github.com>
2023-08-05 11:36:56 +0800

    composer

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('display_errors', true);
+ini_set('error_reporting', E_ALL);
+
+
+
+/*// 中文转拼音
+require_once './vendor/autoload.php';
+
+$obj = new Overtrue\Pinyin\Pinyin();
+$str = $obj->permalink('好像带你去浪漫的土耳其');
+var_dump($str);*/
+
+
+
+
+
+
+
+
+
+
+
